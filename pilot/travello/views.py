5a0d9afd63a2683e108439e932cccf0bc70d13fa
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-06-25 01:40:43 +0300

    making fields dynamic

--- pilot/travello/views.py
+++ pilot/travello/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    dest1 = Destination()
+    dest1.name = "Nairobi"
+    dest1.desc = "Kesho tuko maandamano"
+    dest1.price = 1000
+
+    dest2 = Destination()
+    dest2.name = "Mombasa"
+    dest2.desc = "Zakayo lazima atashuka"
+    dest2.price = 800
+    return render(request, "index.html", {'dest1': dest1, "dest2": dest2})
+

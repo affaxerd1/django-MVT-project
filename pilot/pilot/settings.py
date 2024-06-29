da08188794035e8a9b51c18139d3bf3b1f5a166e
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-06-29 17:51:53 +0300

    creating admin site and fetching items from db

--- pilot/pilot/settings.py
+++ pilot/pilot/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'pilot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'affaxerd',
+        'USER': 'ralph',
+        'PASSWORD': 'a13366551',
+        'HOST': 'localhost'
     }
 }
 
@@ -126,3 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- pilot/travello/views.py
+++ pilot/travello/views.py
@@ -3,14 +3,8 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1 = Destination()
-    dest1.name = "Nairobi"
-    dest1.desc = "Kesho tuko maandamano"
-    dest1.price = 1000
+    dests=Destination.objects.all()
 
-    dest2 = Destination()
-    dest2.name = "Mombasa"
-    dest2.desc = "Zakayo lazima atashuka"
-    dest2.price = 800
-    return render(request, "index.html", {'dest1': dest1, "dest2": dest2})
+
+    return render(request, "index.html", {'dests': dests})
 
